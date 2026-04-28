cd14e52852fda90cf76a8d8ecfc4c8ab7a71af95
Jason Lo <github@jasonlo.email>
2026-04-28 09:57:59 -0700

    Use st_mtime

--- app/util/cache.py
+++ app/util/cache.py
@@ -26,19 +26,17 @@ async def cache_or_run(
     # see if it exists and is fresh
     # if so, return it
     if data_path.exists():
-        async with async_open(data_path, "r") as f:
-            cached_json = json.loads(await f.read())
-            cached_time: int | None = cached_json.get("time")
-            cached_data: T | None = cached_json.get("data")
+        modified_time = int(data_path.stat().st_mtime)
+        logger.debug(f"Cache file {cache_key} modified at {modified_time}")
+        if current_time - modified_time <= cache_time_seconds:
+            async with async_open(data_path, "r") as f:
+                cached_json = json.loads(await f.read())
+                cached_data: T | None = cached_json.get("data")
 
-            if (
-                cached_time is not None
-                and current_time - cached_time <= cache_time_seconds
-                and cached_data is not None
-            ):
-                coroutine.close()
-                logger.debug(f"Cache hit for {cache_key}")
-                return cached_data, cached_time
+                if cached_data is not None:
+                    coroutine.close()
+                    logger.debug(f"Cache hit for {cache_key}")
+                    return cached_data, modified_time
 
     # otherwise, run the coroutine and cache the result
     result = await coroutine
